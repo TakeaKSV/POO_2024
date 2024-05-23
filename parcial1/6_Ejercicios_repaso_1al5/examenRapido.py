f7256c1de5d3a38212445814d6e8156f4e36c411
TakeaKSV <169074465+TakeaKSV@users.noreply.github.com>
2024-05-23 12:42:18 -0600

    examen D:

---
+++ parcial1/6_Ejercicios_repaso_1al5/examenRapido.py
@@ -0,0 +1,12 @@
+nombre=str(input("Ingresa el nombre del alumno: "))
+carrera=str(input("Ingresa la carrera del alumno: "))
+par1=float(input("Ingrese la primer calificacion parcial del alumno: "))
+par2=float(input("Ingrese la segunda calificacion parcial del alumno: "))
+par3=float(input("Ingrese la tercera calificacion parcial del alumno: "))
+projfin=float(input("Ingrese la calificacion final del alumno: "))
+
+cap =print("¿Desea realizar otra captura: (SI/NO)?")
+
+
+
+
