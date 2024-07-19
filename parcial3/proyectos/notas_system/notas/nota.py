37c5ebbdc4d5c85bb12b60c29ae6c4ece9aa2fb3
TakeaKSV <cesar_3141230079@utd.edu.mx>
2024-07-19 08:27:38 -0600

    ER Parcial3 Notas_System

---
+++ parcial3/proyectos/notas_system/notas/nota.py
@@ -0,0 +1,23 @@
+
+from conexionBD import *
+
+class Nota:
+    def __init__(self, usuario_id, titulo, descripcion):
+        self.usuario_id = usuario_id
+        self.titulo = titulo
+        self.descripcion = descripcion
+
+    def crear(self):
+        return 0
+
+    
+    def mostrar(usuario_id):
+        return 0
+
+    
+    def actualizar(id, titulo, descripcion):
+       return 0
+    
+    def eliminar(id):
+        return 0
+        
